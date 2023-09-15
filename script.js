90d39349e3a734c443ae62c4714474af6a78b897
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-09-15 11:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  // Create a frequency map to count character occurrences
+  const charCount = {};
+
+  // Iterate through the string and populate the frequency map
+  for (const char of str) {
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  // Iterate through the string again to find the first non-repeated character
+  for (const char of str) {
+    if (charCount[char] === 1) {
+      return char; // Return the first non-repeated character
+    }
+  }
+
+  // Return null if there are no non-repeated characters
+  return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+// Examples
+console.log(firstNonRepeatedChar('aabbcdd')); // 'c'
+console.log(firstNonRepeatedChar('aabbcc'));  // null
